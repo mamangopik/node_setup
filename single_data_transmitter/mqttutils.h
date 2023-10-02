f81c862258b4ba5c8fd760337ccf4f3f38ce21d8
mamangopik <hidayat.12.software@gmail.com>
2023-10-02 13:51:26 +0700

    add more object payload for single data transmitter

--- single_data_transmitter/mqttutils.h
+++ single_data_transmitter/mqttutils.h
@@ -10,12 +10,12 @@ void reconnect() {
 }
 
 void publish_buffer() {
-  String json_data;
-  json_data = String(random(31,32));
-
   sensor_topic.trim();
   char buf_sensor_topic[100];
   sensor_topic.toCharArray(buf_sensor_topic, sensor_topic.length() + 1);
-  
+
+  String json_data;
+  json_data = "{\"id\":"+String(id_data)+",\"sensor_type\":\"single_data\",\"value\":" + String(random(25,32)) + ",\"label\":\"temperature\"}";
   client.publish(buf_sensor_topic, json_data.c_str());
+  id_data ++;
 }
